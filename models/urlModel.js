f99759244f2ac9db9560692c235e2aac1fb9a246
RameshNaik9 <lahorirameshnaik@gmail.com>
2024-07-14 01:51:10 +0530

    urlModel

--- models/urlModel.js
+++ models/urlModel.js
@@ -13,5 +13,5 @@ const urlSchema = new mongoose.Schema({
   visitHistory: [{ timestamp: { type: Number } }],
 }, { timestamps: true });
 
-const URL = mongoose.model('url', urlSchema);
+const URL = mongoose.model('urls', urlSchema);
 module.exports = URL;
